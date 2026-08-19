3a42bfe21129a2be2aeb0e6dbe0d296ee1d50914
MarsIfeanyi <marschalice@gmail.com>
2026-08-19 12:02:51 +0100

    Incrementally load the products table

---
+++ walmart_dataeng_project/models/silver_t/products_t.sql
@@ -0,0 +1,21 @@
+-- Implementing Incremental Data Loading
+
+{{ config(materialized='incremental', unique_key='product_id') }}
+
+
+SELECT
+    *,
+    current_timestamp() AS processed_at
+FROM 
+    {{source('walmart_databricks', 'products')}}
+
+-- Applying incremental filter
+{% if is_incremental() %}
+    WHERE updated_timestamp > (
+        SELECT COALESCE(MAX(updated_timestamp), 
+        '1900-01-01'
+        )
+        FROM {{this}}
+        )
+    -- {{this}} = Dbt model target table
+{% endif %}
